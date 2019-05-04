65e1603cdb6419db99e2cbf44632c17d09ab65c0
jonah.wichtrup <jonah@wichtrup-li.de>
2019-05-04 23:48:42 +0200

    clock test

--- LedMatrixOS/Application.cs
+++ LedMatrixOS/Application.cs
@@ -22,21 +22,56 @@ namespace LedMatrixOS
             moduleLoader.LoadModules();
             
             Controls.AddScroller("MainScroller", "P1Pin36", "P1Pin38");
+
+            ScrollLayout layout = new ScrollLayout("da");
             
-            rect = new Rectangle(0, 0, 31, 16, CanvasColor.YELLOW);
-            Rectangle rect1 = new Rectangle(0, 0, 16, 16, CanvasColor.GREEN);
+            AnimatedChar hour1 = new AnimatedChar('0');
+            AnimatedChar hour2 = new AnimatedChar('0');
+            AnimatedChar dot = new AnimatedChar(':');
+            AnimatedChar minute1 = new AnimatedChar('0');
+            AnimatedChar minute2 = new AnimatedChar('0');
+            AnimatedChar dot2 = new AnimatedChar(':');
+            AnimatedChar second1 = new AnimatedChar('0');
+            AnimatedChar second2 = new AnimatedChar('0');
 
-            StackPanel stack = new StackPanel();
+            StackPanel stackPanel = new StackPanel();
+            StackPanel stackPanel2 = new StackPanel();
+            StackPanel stackPanel3 = new StackPanel();
 
-            stack.Children.Add(rect1);
-            stack.Children.Add(rect);
 
-            //ScrollLayout scrollLayout = new ScrollLayout("MainScroller");
+            stackPanel.Orientation = Orientation.Horizontal;
+            stackPanel3.Orientation = Orientation.Horizontal;
+            
+            stackPanel.Add(hour1);
+            stackPanel.Add(hour2);
+            stackPanel.Add(dot);
+            stackPanel.Add(minute1);
+            stackPanel.Add(minute2);
+            
+            
+            stackPanel3.Add(second1);
+            stackPanel3.Add(second2);
+            
+            
+            stackPanel2.Add(stackPanel);
+            stackPanel2.Add(stackPanel3);
+            
+            Child = stackPanel2;
 
-            //scrollLayout.Child = stack;
-
-            Child = stack;
-
+            Timer t = new Timer();
+            t.Elapsed += (s, a) =>
+            {
+                var time = DateTime.Now.ToString("HH:mm:ss");
+                
+                hour1.ChangeChar(time[0]);
+                hour2.ChangeChar(time[1]);
+                minute1.ChangeChar(time[3]);
+                minute2.ChangeChar(time[4]);
+                second1.ChangeChar(time[6]);
+                second2.ChangeChar(time[7]);
+            };
+            t.Interval = 1000;
+            t.Start();
         }
     }
 }
